ccb004305bd00fb34ce2311ee563bbc7c0f0483b
GrishaMagic <grisha.berezin.1998@mail.ru>
2018-02-09 11:52:31 -0800

    Create all directives.\nStable config file.\nStable shell file.

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -s /home/grishamagic/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/grishamagic/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
