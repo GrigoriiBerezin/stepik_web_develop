32eae5ffd097528de2d3f33bff7fd2d902ef5bf7
GrishaMagic <grisha.berezin.1998@mail.ru>
2018-02-12 01:42:19 -0800

    Try to make gunicorn works

--- init.sh
+++ init.sh
@@ -1,12 +1,11 @@
 #To config NGINX
 sudo ln -s /home/grishamagic/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo service nginx restart
 
 #To config Gunicorn
-sudo mkdir /etc/gunicorn.d
 sudo ln -s /home/grishamagic/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c /etc/gunicorn.d/test /home/grishamagic/web/hello:application_func &
+sudo service gunicorn restart
 
 #To config MySQL
-sudo /etc/init.d/mysql start 
+sudo service mysql restart
