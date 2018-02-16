93433a7ee358dc190d56a5e8b2350bba7749dad8
Grigory Berezin <grisha.berezin.1998@mail.ru>
2018-02-16 04:44:40 -0800

    Create django app

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #To config NGINX
-sudo ln -s /home/grishamagic/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
 #To config Gunicorn
-sudo ln -s /home/grishamagic/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/test_django
 sudo service gunicorn restart
 
 #To config MySQL
