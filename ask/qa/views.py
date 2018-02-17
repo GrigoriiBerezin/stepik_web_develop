453f66bcd339f56a4963162df63b142cfc4a8305
Grigory Berezin <grisha.berezin.1998@mail.ru>
2018-02-17 09:32:16 -0800

    Error fixes with urls and views

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponce
+from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
-    return HttpResponce('OK')
+    return HttpResponse('OK')
