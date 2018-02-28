ae91eeb68b8388798e059b4630e4e03367d867fb
Grigory Berezin <grisha.berezin.1998@mail.ru>
2018-02-27 21:55:18 -0800

    Working MySql database

--- init.sh
+++ init.sh
@@ -11,4 +11,6 @@ sudo service gunicorn restart
 #To config MySQL
 sudo service mysql restart
 sudo mysql -u root -e "CREATE DATABASE django_app"
-sudo mysql -u root -e "CREATE USER 'django_root@localhost' IDENTIFIED BY 'GrishaBerezin1998'"
+sudo mysql -u root -e "CREATE USER 'django_root' IDENTIFIED BY 'root'"
+sudo mysql -u root -e "GRANT ALL ON django_app.* TO 'django_root'"
+sudo ln -s /home/box/web/etc/mysql.conf /etc/mysql/my.cnf
