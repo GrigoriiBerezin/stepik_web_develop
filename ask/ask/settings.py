328f8381341652e46c19512e3d6564c8cacdbb1f
Grigory Berezin <grisha.berezin.1998@mail.ru>
2018-03-11 06:20:10 -0700

    Create fixtures files

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Fixtures files to autoload into database
+# https://docs.djangoproject.com/en/1.11/howto/initial-data
+
+FIXTURE_DIRS = [
+    BASE_DIR + '/fixtures',
+]
